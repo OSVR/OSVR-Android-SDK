0c2376dd7f2dd970049a36b8c45b82669cca8243
Ryan Pavlik <ryan@sensics.com>
2015-07-09 11:52:47 -0500

    Clean up install-snapshots, and don't directly package the files there.

--- install-snapshots.sh
+++ install-snapshots.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -e
 
-# Given a string describing a matrix build like ANDROID_ABI=armeabi-v7a,label=ndk
-# on the pipeline, get the named field.
+# Given a string describing a matrix build as Jenkins Copy-Artifacts does
+# (like ANDROID_ABI=armeabi-v7a,label=ndk) on the pipeline, get the named field.
+# Example: echo ANDROID_ABI=armeabi-v7a,label=ndk | getMatrixField ANDROID_ABI
+# outputs armeabi-v7a to stdout.
 getMatrixField() {
     tr ',' '\n' | grep "$1" | cut --only-delimited -d = -f 2
 }
@@ -18,8 +20,4 @@ for dir in *; do
     echo "Detected ABI: $abi"
     mv $dir/build/install $builds/$abi
 done
-
-echo " ---------- Compressing!"
-cd $(dirname $0)
-tar -jcvf osvr-android-ndk.tar.bz2 NDK
 )
