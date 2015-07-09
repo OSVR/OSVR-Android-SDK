58dcfdc70c9b25afd2d833cbeb2f68bc20d2a258
Ryan Pavlik <ryan@sensics.com>
2015-07-09 12:11:33 -0500

    Build up ABI list.

--- install-snapshots.sh
+++ install-snapshots.sh
@@ -8,16 +8,30 @@ getMatrixField() {
     tr ',' '\n' | grep "$1" | cut --only-delimited -d = -f 2
 }
 
+ROOT=$(cd $(dirname $0) && pwd)
 
 (
-cd $(dirname $0)
+cd $ROOT
 builds=$(cd NDK/osvr/builds && pwd)
-
+abis=""
 cd snapshots
 for dir in *; do
     abi=$(echo $dir | getMatrixField ANDROID_ABI)
     echo "Moving build: $dir"
     echo "Detected ABI: $abi"
+    abis="${abis} $abi"
     mv $dir/build/install $builds/$abi
+done
+
+cd $ROOT
+
+# Record list of ABIs
+echo "OSVR_ABIS := $abis" > NDK/osvr_abis.mk
+
+# Update all the tests with the full ABI list.
+cd NDK-tests
+for dir in *; do
+    appmk="${dir}/jni/Application.mk"
+    echo "APP_ABI := $abis" >> $appmk
 done
 )
