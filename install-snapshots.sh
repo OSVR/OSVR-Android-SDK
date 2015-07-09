3d8a16a08bfa892e23def671946c37b1dd05751b
Ryan Pavlik <ryan@sensics.com>
2015-07-09 09:28:36 -0500

    Adjust moving logic to handle how Jenkins actually names matrix build artifacts.

--- install-snapshots.sh
+++ install-snapshots.sh
@@ -1,12 +1,22 @@
 #!/bin/sh -e
+
+# Given a string describing a matrix build like ANDROID_ABI=armeabi-v7a,label=ndk
+# on the pipeline, get the named field.
+getMatrixField() {
+    tr ',' '\n' | grep "$1" | cut --only-delimited -d = -f 2
+}
+
+
 (
 cd $(dirname $0)
 builds=$(cd NDK/osvr/builds && pwd)
 
 cd snapshots
-for abi in *; do
-    echo "Moving build for ABI $abi"
-    mv $abi/build/install $builds/$abi
+for dir in *; do
+    abi=$(echo $dir | getMatrixField ANDROID_ABI)
+    echo "Moving build: $dir"
+    echo "Detected ABI: $abi"
+    mv $dir/build/install $builds/$abi
 done
 
 echo " ---------- Compressing!"
