0a5e50db140eceed6e3845b40020d077fcb25ce4
Ryan Pavlik <ryan@sensics.com>
2015-07-09 12:58:11 -0500

    Use sh2ju in running the tests.

--- run-ndk-tests.sh
+++ run-ndk-tests.sh
@@ -1,8 +1,12 @@
-#!/bin/sh -e
+#!/bin/sh
+
+#Include sh2ju
+. $(dirname $0)/sh2ju/sh2ju.sh
+
 (
 cd $(dirname $0) && cd NDK-test
+juLogClean
 for dir in *; do
-    echo "-------------- $dir --------------"
-    ndk-build NDK_LOG=1 V=1 -C $dir
+    juLog -name=$dir               ndk-build NDK_LOG=1 V=1 -C $dir
 done
 )
