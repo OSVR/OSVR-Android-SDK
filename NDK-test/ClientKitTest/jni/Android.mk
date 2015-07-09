3844939b6b5e55fa973865356e377156b2593f24
Ryan Pavlik <ryan@sensics.com>
2015-07-08 19:43:56 -0500

    Move the test.

--- NDK-test/ClientKitTest/jni/Android.mk
+++ NDK-test/ClientKitTest/jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := dummy
 LOCAL_SRC_FILES := dummy.cpp
 LOCAL_SHARED_LIBRARIES := osvrClientKit
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
 
-$(call import-add-path,../NDK)
+$(call import-add-path,../../NDK)
 $(call import-module,osvr/ClientKit)
