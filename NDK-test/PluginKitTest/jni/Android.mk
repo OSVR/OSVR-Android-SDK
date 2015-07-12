98bb9df1f50a1bf70f5568252f2ebab5ede1918a
Ryan Pavlik <ryan@sensics.com>
2015-07-12 05:40:56 -0500

    PluginKit module.

---
+++ NDK-test/PluginKitTest/jni/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := dummy
+LOCAL_SRC_FILES := dummy.cpp
+LOCAL_SHARED_LIBRARIES := osvrPluginKit
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-add-path,../../NDK)
+$(call import-module,osvr/PluginKit)
